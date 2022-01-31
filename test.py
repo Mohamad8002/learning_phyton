f17cab90e1c82f741ba26a9b3e14d07ac81a7ccf
Mohamad8002 <dehban1382@gmail.com>
2022-01-31 18:05:12 +0330

    Trial

---
+++ test.py
@@ -0,0 +1,49 @@
+                                                                                            # CTRL_S 
+# print('')______________________________________________________________ the first of command phyton
+# print('mohamad')
+# print(259)
+# type()____________________________________________________________________ To specify the data type
+# print(type(23.5))
+# print(type(1258))
+# mark= float(22)
+# print(mark)
+# print(type(mark))
+# d=17.4
+# print(type(d))
+# z = mark+d
+# print(z)
+# for i in range(3):
+#     print(i)
+
+
+# number = int(input("enter intger number"))
+# factorial = 1
+# for i in range(1,number+1):
+#     factorial = factorial * i
+#     print(factorial)
+# A="""my
+# name
+# is :
+# mohamad"""
+# print(A)
+# A='ali is: \"fast\"'
+# print(A)
+# A='ali go to \ngym'
+# print(A)
+# A='ali go\tto\tgym\ttoday'
+# print(A)
+# A='hosain watch tv  \btonight'
+# print(A)
+# A='chalenger'
+# print(A[-7:-1])
+# print(A[2:])
+# print(A[:6])
+# print(A[2:8])
+age=18
+A='hosain'
+B='mohamad'
+p=B+A+str(age)
+D=f'{B}{A}{age}'
+
+print(p)
+print(D)
