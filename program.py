5220d851e70b80db8c26822a2519ca77da944d61
Mohamad8002 <dehban1382@gmail.com>
2022-02-22 16:37:23 +0330

    trial

---
+++ program.py
@@ -0,0 +1,82 @@
+# number = int(input("enter intger number"))
+# factorial = 1
+# if number==0 or number==1:
+#     print(1)
+# for i in range(1,number+1):
+#     factorial = factorial * i
+#     print(factor)
+
+# for i in range (1,100):
+#     if i% 2 ==1:
+#         print(i)
+
+# for i in range(1000):
+#     if i %7==0:
+#         print(i)
+# for i in range(0,100,2):
+#     print(i)
+
+
+
+# age_son=int(input(''))
+# age_dad=int(input(''))
+# if age_son>18 and age_dad>30:
+#     if age_son>25 or age_dad>50:
+#         print('ok')
+# else:
+#     print('not ok')
+# fizik=int(input(''))
+# math=int(input(''))
+# chemestry=int(input(''))
+# ave=(fizik+math+chemestry)/3
+# if ave>17 or (math>16 and chemestry>16 and fizik>16) or math==20:
+#     print('ok')
+# else:
+#     print('
+
+
+
+
+# list1=[]
+# for i in range (3):
+#     number=int(input())
+#     fact=1
+#     for i in range (1,number+1):
+#         fact=fact*i
+#     if number**3>fact:
+#         list1.append(number)
+# print(list1)
+
+
+
+for i in range(10):
+    for y in range(10):
+       print(i,"*",y,"=",y*i)
+
+
+
+
+
+
+# number=int(input())
+# while number<10:
+#     number=int(input())
+#     print("hello")
+
+
+
+# a='mohaMad Dehban text y y {} {} {}  '
+# print(a.count(' '))
+# print(a.format('{a}','{b}','{c}'))
+# print(a.capitalize())
+# print(a.casefold())
+# print(a.find('D'))
+# print(a.lower())
+# print(a.upper())
+
+
+
+
+
+# a=str(23)
+# print(type(a))
